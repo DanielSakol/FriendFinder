6aec8e8bfddacc55cc7464d2661da79e80fd845f
Danny Acne <dannyacne@gmail.com>
2019-04-23 11:07:36 -0500

    commit4

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 // require packages
 var express = require("express");
 var path = require("path");
-var handlebars = require("handlebars");
-  
+
+var app = express();
+var PORT = 7200;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+  });
+
+// listen
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
