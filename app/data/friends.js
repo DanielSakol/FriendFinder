081d9d57e8f1962cbf9463ad82da00458386882f
Danny Acne <dannyacne@gmail.com>
2019-05-08 12:23:31 -0500

    new commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,32 +2,32 @@ var friends = [
 	{
 		name: "R.P. McMurphy",
 		photo: "https://images-na.ssl-images-amazon.com/images/I/41K3liuu5VL._SY355_.jpg",
-		scores: [5, 1, 4, 4, 3, 5, 3, 3, 3, ]
+		scores: [5, 1, 3, 5, 4, 1, 5, 1, 2, 3, ]
 	},
 	{
 		name: "Martini",
 		photo: "https://assets.mycast.io/characters/7499_normal.jpg?1506782378",
-		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+		scores: [2, 3, 1, 1, 3, 4, 3, 1, 4, 3]
 	},
 	{
 		name: "Dale Harding",
 		photo: "https://oneflewoverthecuckoos.weebly.com/uploads/2/5/6/3/25639630/934049009.jpg?425",
-		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+		scores: [3, 1, 4, 4, 1, 1, 3, 4, 2, 4]
 	},
 	{
 		name: "Charlie Cheswick",
 		photo: "https://robertslooneybin.weebly.com/uploads/3/7/7/3/37730841/2104192_orig.jpg",
-		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+		scores: [3, 2, 3, 1, 3, 4, 3, 2, 5, 3]
 	},
 	{
 		name: "Chief Bromden",
 		photo: "https://usercontent2.hubstatic.com/13641207_f520.jpg",
-		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+		scores: [1, 3, 4, 4, 1, 1, 2, 5, 2, 5]
     },
     {
 		name: "Billy Bibbit",
 		photo: "https://behindtheasylumwalls.weebly.com/uploads/2/5/7/1/25719689/3992749.jpg?253",
-		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+		scores: [2, 1, 1, 1, 5, 2, 1, 3, 2, 2]
 	},
 ];
 
